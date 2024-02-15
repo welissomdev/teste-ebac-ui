7fabdbfab20c2d7f74fbcde8b5dae06e04062eb9
welissom <welyssom155@gmail.com>
2024-02-15 16:26:11 -0300

    lista

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress"/>
+
+describe('funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    }); 
+
+    it('Deve selecionar um produto da lista ', () => {
+        cy.get('.product-block ')
+        //.first()
+        //.last()
+        //.eq(2)
+        .contains('Abominable Hoodie')
+        .click()
+        cy.get('#tab-description > :nth-child(1)').should('contain','It took CoolTech™ weather apparel know-how and lots of wind-resistant fabric to get the Abominable Hoodie just right.')
+    });
+
+});
