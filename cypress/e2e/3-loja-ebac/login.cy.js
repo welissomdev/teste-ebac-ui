e7725b14c722e776f36eeb88d4a5b1f9463a1fe9
welissom <welyssom155@gmail.com>
2024-02-15 14:08:17 -0300

    testes negativos e hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,12 +1,35 @@
 /// <reference types="cypress"/>
 
-describe('funcionalidade: login', () =>(
-    it('Deve fazer login com sucesso', () => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-    cy.get('#username').type('welyssom@gmail.com')
-    cy.get('#password').type('qawsedrftgyhujikolpç')
-    cy.get('.woocommerce-form > .button').click()
+describe('funcionalidade: login', () => {
 
-    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, welyssom (não é welyssom? Sair)')
+beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+});
+
+afterEach(() => {
+    cy.screenshot()
+});
+
+    it('Deve fazer login com sucesso', () => {
+        cy.get('#username').type('welyssom@gmail.com')
+        cy.get('#password').type('qawsedrftgyhujikolpç')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, welyssom (não é welyssom? Sair)')
     })
-))
+
+    it('Deve exibir uma mensagem de erro ao inserir usuário invalido', () => {
+        cy.get('#username').type('welysom@gmail.com')
+        cy.get('#password').type('qawsedrftgyhujikolpç')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('exist')  
+    });
+ 
+    it('Dve exibir uma mensagem de erro ao inserir senha invalida', () => {
+        cy.get('#username').type('welyssom@gmail.com')
+        cy.get('#password').type('awsedrftgyhujikolpç')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('exist')
+    });
+  
+})
